957719be825355d9d23578834dca40bc11ea0d17
zhiwar404M <97038061+zhiwar404M@users.noreply.github.com>
2025-06-30 00:41:51 +0300

    Create translation.js

---
+++ translation.js
@@ -0,0 +1,18 @@
+const translations = {
+  "welcome_title": {
+    "ku": "بەخێرهاتی بۆ | وێبسایتی فەرمی ZHIWAR",
+    "ar": "مرحبًا بكم | الموقع الرسمي ZHIWAR",
+    "en": "Welcome | Official ZHIWAR Website"
+  },
+  "welcome_text": {
+    "ku": "بەرنامەکان بە تەواوی دارێژراوە بە زمانی کوردی",
+    "ar": "التطبيقات مبرمجة بالكامل باللغة الكردية",
+    "en": "Apps fully programmed in Kurdish language"
+  },
+  "apps_section": {
+    "ku": "بەرنامەکان",
+    "ar": "التطبيقات",
+    "en": "Applications"
+  },
+  // زیادکردنی هەموو دەقەکانی تر لێرە...
+};
